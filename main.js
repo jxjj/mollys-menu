b7cf375e0f0fcfee083965c143eb6035baed6ec0
James Johnson <johnsojr@gmail.com>
2017-06-24 18:14:45 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function renderPageHeader() {
+	const h1 = document.querySelector('h1');
+	const [w0, w1, w2, w3, ...rest] = h1.innerText.split(' ');
+
+	h1.innerHTML = `
+		<span class="line-1">${w0} ${w1} ${w2}</span>
+		<span class="line-2">${w3}</span>
+		<span class="line-3">${rest.join(' ')}</span>
+		<span class="and-video">&amp; video game emporium</span>
+	`;
+}
+
+function init() {
+	renderPageHeader();
+}
+
+ready(init);
