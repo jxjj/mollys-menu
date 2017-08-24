3922843fbf8e2b07432f41a0991660573a1219d3
James Johnson <johnsojr@gmail.com>
2017-08-23 22:18:06 -0500

    eslint. added backgroung image

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+/* global document */
+
 function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+  if (
+    document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading'
+  ) {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
@@ -7,19 +11,19 @@ function ready(fn) {
 }
 
 function renderLogo() {
-	const h1 = document.querySelector('h1');
-	const [w0, w1, w2, w3, ...rest] = h1.innerText.split(' ');
+  const h1 = document.querySelector('h1');
+  const [w0, w1, w2, w3, ...rest] = h1.innerText.split(' ');
 
-	h1.innerHTML = `
-		<span class="line-1">${w0} ${w1} ${w2}</span>
-		<span class="line-2">${w3}</span>
-		<span class="line-3">${rest.join(' ')}</span>
-		<span class="and-video">&amp; video game emporium</span>
-	`;
+  h1.innerHTML = `
+    <span class="line-1">${w0} ${w1} ${w2}</span>
+    <span class="line-2">${w3}</span>
+    <span class="line-3">${rest.join(' ')}</span>
+    <span class="and-video">&amp; video game emporium</span>
+  `;
 }
 
 function init() {
-	renderLogo();
+  renderLogo();
 }
 
 ready(init);
